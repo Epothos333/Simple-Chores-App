ed787d9342be1790a4caa6ea1da8610a8e877769
Edwin A <ea976@nyu.edu>
2016-02-23 14:53:03 -0500

    Added more info button, styled a bit, added much of the Details functionality but still missing the view on Details

--- app.js
+++ app.js
@@ -2,20 +2,28 @@
 
 app.controller('listController', function() {
 	var vm=this;
-	vm.listItems = [];
+	vm.listItems = ['Example 1', 'Example 2', 'Example 3'];
 	vm.removeButtons =[];
+	vm.detailsArr = [];
 	vm.toDoThing;
+	vm.details;
+
 
 	vm.adding = function() {
 		if (vm.toDoThing){
-		vm.listItems.push(vm.toDoThing);
-		document.getElementById('toDoInput').value = '';
-		vm.toDoThing = '';
-	};
-	};
+			vm.listItems.push(vm.toDoThing);
+			document.getElementById('toDoInput').value = '';
+			vm.toDoThing = '';
+			};
+		if (vm.details){
+			vm.detailsArr.push(vm.details);
+			document.getElementById('choreDetail').value = '';
+			vm.details = '';
+		}
+		};
 	vm.removeItem = function(index) {
 		vm.listItems.splice(index ,1);
-	}
+		}
 
 });
 
