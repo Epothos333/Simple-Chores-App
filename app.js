86ef0b46575bc35ce111444e124d04b33b1c0823
Edwin A <ea976@nyu.edu>
2016-02-24 00:23:02 -0500

    Made a Class for chore names/details, switched lists for divs, details now on side of page, displaying one at a time. removed the good styling. Needs lots of styling and specific Details. probably should add a form.

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
 	var app = angular.module('toDoListModule', []);
 
 app.controller('listController', function() {
+	var Chore = function(chore, detail) {
+		this.item = chore;
+		this.detail = detail;
+	};
 	var vm=this;
-	vm.listItems = ['Example 1', 'Example 2', 'Example 3'];
+	vm.listItems = [
+		{
+			item: 'Example 1',
+			detail: 'Detail 1'
+		},
+		{
+			item: 'Example 2',
+			detail: 'Detail 2'
+		},
+		{
+			item: 'Example 3',
+			detail: 'Detail 3'
+		}];
 	vm.removeButtons =[];
-	vm.detailsArr = [];
 	vm.toDoThing;
-	vm.details;
+	vm.detailInfo;
 
 
 	vm.adding = function() {
+		var itemToAdd = new Chore(vm.toDoThing, vm.detailInfo);
+
+		if(!vm.detailInfo) {
+			itemToAdd.detail = 'No info';
+		}
 		if (vm.toDoThing){
-			vm.listItems.push(vm.toDoThing);
+			vm.listItems.push(itemToAdd);
 			document.getElementById('toDoInput').value = '';
 			vm.toDoThing = '';
 			};
-		if (vm.details){
-			vm.detailsArr.push(vm.details);
-			document.getElementById('choreDetail').value = '';
-			vm.details = '';
-		}
 		};
 	vm.removeItem = function(index) {
 		vm.listItems.splice(index ,1);
+		}
+
+	vm.showDetails = function(index) {
+		var showThis = vm.listItems[index].detail;
+		var para = document.getElementById('detail');
+		para.innerHTML = showThis;
 		}
 
 });
