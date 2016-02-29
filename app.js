50ec4d2d7f830d9a4d2b42194745bd9b7c3e79d9
Edwin A <ea976@nyu.edu>
2016-02-29 10:10:37 -0500

    Added Date/Time/Notes and some style changes for Details.

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
 	var app = angular.module('toDoListModule', []);
 
 app.controller('listController', function() {
-	var Chore = function(chore, detail) {
+	var Chore = function(chore, date, times, notes) {
 		this.item = chore;
-		this.detail = detail;
+		this.date = date;
+		this.times = times;
+		this.notes = notes;
 	};
 	var vm=this;
 	vm.listItems = [
 		{
 			item: 'Example 1',
-			detail: 'Detail 1'
+			date: '01/01/99',
+			times: '1:00 P.M.',
+			notes: 'notes 1'
 		},
 		{
 			item: 'Example 2',
-			detail: 'Detail 2'
+			date: '02/02/92',
+			times: '2:00PM',
+			notes: 'notes 2'
 		},
 		{
 			item: 'Example 3',
-			detail: 'Detail 3'
+			date: '03/03/93',
+			times: '3:00PM',
+			notes: 'notes 3'
 		}];
-	vm.removeButtons =[];
-	vm.toDoThing;
-	vm.detailInfo;
 
+	var toDoThing = '';
+	var date = '';
+	var times = '';
+	var notes = '';
 
 	vm.adding = function() {
-		var itemToAdd = new Chore(vm.toDoThing, vm.detailInfo);
+		vm.date = vm.date.toString().split("").splice(0,15).join("");
 
-		if(!vm.detailInfo) {
-			itemToAdd.detail = 'No info';
+		var itemToAdd = new Chore(vm.toDoThing, vm.date, vm.times, vm.notes);
+
+		if(!vm.date) {
+			itemToAdd.detail = 'Not Specified';
+		}
+		if(!vm.times) {
+			itemToAdd.times = 'Not Specified';
+		}
+		if(!vm.notes) {
+			itemToAdd.notes = 'Not Specified';
 		}
 		if (vm.toDoThing){
 			vm.listItems.push(itemToAdd);
@@ -41,9 +58,11 @@ app.controller('listController', function() {
 		}
 
 	vm.showDetails = function(index) {
-		var showThis = vm.listItems[index].detail;
+		var showDate = '<p class="makeBlock"><span class="boldMe"> Date Due: </span>' + vm.listItems[index].date + '</span></p>';
+		var showTime =  '<p class="makeBlock"><span class="boldMe"> Time Due: </span>' + vm.listItems[index].times + '</span></p>';
+		var showNotes =  '<p class="makeBlock"><span class="boldMe"> Notes: </span>' + vm.listItems[index].notes + '</span></p>';
 		var para = document.getElementById('detail');
-		para.innerHTML = showThis;
+		para.innerHTML = showDate + showTime + showNotes;
 		}
 
 });
